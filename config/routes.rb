14687836adb830d119bfe1960c05a57b89182220
theodorepgl <limlimtheo96@gmail.com>
2023-10-25 13:22:33 +0800

    Add Controller and Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'bookings/index'
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/show'
+  get 'bookings/edit'
+  get 'bookings/update'
+  get 'users/new'
+  get 'users/create'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "home#index"
+  root "welcome#index"
+  resources :users, only: [:new, :create]
+  # resources :sessions, only: [:new, :create, :destroy]
+  resources :bookings, except: [:destroy] do
+    resources :reviews, only: [:new, :create]
+  end
 end
