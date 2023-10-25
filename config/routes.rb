a3b54abb3cc58523734ffe7833241e97dc7563e6
theodorepgl <limlimtheo96@gmail.com>
2023-10-25 15:49:22 +0800

    Booking new

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/show'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'users/new'
-  get 'users/create'
   devise_for :users
   root "welcome#index"
   resources :users, only: [:new, :create]
