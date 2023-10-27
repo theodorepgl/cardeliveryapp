f74027eb7424a4edea779bc65eb4d7a21058c14c
theodorepgl <limlimtheo96@gmail.com>
2023-10-27 16:09:13 +0800

    Bookings things

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,33 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = current_user.bookings.all
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = current_user.bookings.create(booking_params) 
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:car, :pickup_datetime, :pickup_location, :dropoff_location, :distance, :price)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.create(user_params)
     if @user.save
       redirect_to new_user_path, notice: 'User successfully created!'
     else
