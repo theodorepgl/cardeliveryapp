dc08b0b04a9c55861eac5b8dee50182aa391c437
theodorepgl <limlimtheo96@gmail.com>
2023-10-31 12:12:04 +0800

    Books

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,7 @@ class BookingsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @bookings = current_user.bookings.all
+    @bookings = current_user.bookings
   end
 
   def new
